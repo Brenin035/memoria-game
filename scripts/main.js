b0f1e11add08a50c6f9bd2c992e27b9f07551b4e
Brenin035 <brenin03500@gmail.com>
2025-09-20 12:47:56 -0300

    Atualizando jogo da memoria em desenvolvimento.

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,27 @@ function urlBuider(number){
 
 let  card = new CardManager(urlBuider);
 let board = new BoardManagar("board", 50, card);
+
+
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start');
+
+for (let i = 4; i <= 10; i+2) {
+    let n = i+i;
+
+    let op = document.createElement('option');
+
+    op.value=n;
+    op.innerHTML=n;
+
+    select.appendChild(op);
+
+}
+
+start.addEventListener("click" , () => {
+
+    menu.classList.add("hidden");
+    board.node.classList.remove("hidden");
+    board.fill(select.value);
+});
